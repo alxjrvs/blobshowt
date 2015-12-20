7e5d82a546f06f02a3594ee4b969a13fbc0be515
Alex Jarvis <alxjrvs@gmail.com>
2015-12-19 21:00:17 -0500

    properly nest

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -1,16 +1,20 @@
-class ResponsesController < ApplicationController
+module Api
+  module V1
+    class ResponsesController < ApplicationController
 
-  TWILIO="+17818195082",
-  ME="+17814757398"
+      TWILIO="+17818195082",
+      ME="+17814757398"
 
-  def create
-    puts params
+      def create
+        puts params
+      end
+
+      private
+
+      def twilio
+        @_twilio ||= Twilio::REST::Client.new
+      end
+
+    end
   end
-
-  private
-
-  def twilio
-    @_twilio ||= Twilio::REST::Client.new
-  end
-
 end
