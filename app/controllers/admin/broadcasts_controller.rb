79a3e0b2eb8e061b6fe1bc48e456c4c93ee57c6d
Alex Jarvis <alxjrvs@gmail.com>
2015-12-19 22:19:31 -0500

    Testing views

--- app/controllers/admin/broadcasts_controller.rb
+++ app/controllers/admin/broadcasts_controller.rb
@@ -1,16 +1,43 @@
-class BroadcastsController < AdminController
+module Admin
+  class BroadcastsController < AdminController
 
-  def new
-    @broadcast = Broadcast.new
+    def new
+      @broadcast = Broadcast.new
+    end
+
+    def index
+      @broadcasts = Broadcast.all
+    end
+
+    def create
+      if broadcast.save!
+        RegisteredNumber.all.each do |rn|
+          twilio.messages.create(
+            from: TWILIO,
+            to: rn.number,
+            body: broadcast.body
+          )
+        end
+        flash.now["notice"] = "Broadcast sent"
+        redirect_to :index
+      else
+        flash.now["error"] = "Error occurred!"
+        render :new
+      end
+    end
+
+    private
+
+    def broadcast
+      Broadcast.new(broadcast_params)
+    end
+
+    def twilio
+      @_twilio ||= Twilio::REST::Client.new
+    end
+
+    def broadcast_params
+      params.require(:broadcast).permit(:body)
+    end
   end
-
-  def index
-    @broadcasts = Broadcast.all
-  end
-
-  def create
-  end
-
-  private
-
 end
