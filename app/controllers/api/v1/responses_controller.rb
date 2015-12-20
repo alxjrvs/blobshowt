f81e44287d068f98653f8b85261044ed4466b0d7
Alex Jarvis <alxjrvs@gmail.com>
2015-12-19 21:04:15 -0500

    skip authenticity

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -1,9 +1,10 @@
 module Api
   module V1
     class ResponsesController < ApplicationController
-
       TWILIO="+17818195082",
       ME="+17814757398"
+
+      skip_before_action :verify_authenticity_token, only: [:create]
 
       def create
         puts params
